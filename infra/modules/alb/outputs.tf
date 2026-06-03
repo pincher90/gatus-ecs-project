a0d2b295cb953cc2595572bbe24f1473237bf72c
Jojo Moses <jojomoses@hotmail.com>
2026-06-03 21:11:57 +0100

    Add ALB zone ID output

--- infra/modules/alb/outputs.tf
+++ infra/modules/alb/outputs.tf
@@ -5,3 +5,8 @@ output "alb_dns_name" {
 output "target_group_arn" {
   value = aws_lb_target_group.ecs.arn
 }
+
+output "alb_zone_id" {
+  description = "Route 53 hosted zone ID for the ALB"
+  value       = aws_lb.main.zone_id
+}
