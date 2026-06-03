8ab011a2946aa4f1881f8efbc7d5d56ecc7b9edf
Jojo Moses <jojomoses@hotmail.com>
2026-06-03 21:21:02 +0100

    Add Route 53 alias for ALB

--- infra/dns.tf
+++ infra/dns.tf
@@ -2,3 +2,14 @@ data "aws_route53_zone" "gatus" {
   name         = var.hosted_zone_name
   private_zone = false
 }
+resource "aws_route53_record" "gatus" {
+  zone_id = data.aws_route53_zone.gatus.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = module.alb.alb_dns_name
+    zone_id                = module.alb.alb_zone_id
+    evaluate_target_health = true
+  }
+}
