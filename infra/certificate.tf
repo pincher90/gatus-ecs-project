0e1e4a06e747fb12cbd0b53cb03208b77cfcf253
Jojo Moses <jojomoses@hotmail.com>
2026-06-03 20:41:32 +0100

    Add ACM certificate DNS validation

---
+++ infra/certificate.tf
@@ -0,0 +1,30 @@
+resource "aws_acm_certificate" "alb" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "alb_certificate_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.alb.domain_validation_options :
+    dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.gatus.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.value]
+}
+
+resource "aws_acm_certificate_validation" "alb" {
+  certificate_arn         = aws_acm_certificate.alb.arn
+  validation_record_fqdns = [for record in aws_route53_record.alb_certificate_validation : record.fqdn]
+}
